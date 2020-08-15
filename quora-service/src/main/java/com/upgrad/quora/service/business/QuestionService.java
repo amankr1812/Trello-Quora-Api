8e8a4255fb27537404a0f01f22bb2b3413d7a26c
Dolly99 <67870449+Dolly99@users.noreply.github.com>
2020-08-15 23:14:33 +0530

    Added CreateQuestion functionality
    Signed-off-by: Dolly99 <67870449+Dolly99@users.noreply.github.com>

---
+++ quora-service/src/main/java/com/upgrad/quora/service/business/QuestionService.java
@@ -0,0 +1,70 @@
+package com.upgrad.quora.service.business;
+
+import com.upgrad.quora.service.dao.QuestionDao;
+import com.upgrad.quora.service.dao.UserAuthDao;
+import com.upgrad.quora.service.entity.QuestionEntity;
+import com.upgrad.quora.service.entity.UserAuthEntity;
+import com.upgrad.quora.service.exception.AuthorizationFailedException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class QuestionService {
+
+    @Autowired
+    private QuestionDao questionDao;
+
+    @Autowired
+    private UserAuthDao userAuthDao;
+
+
+    /**
+     * CREATE QUESTION
+     * Signin before creating a question is mandatory
+     * @param QuestionRequest of user to be created
+     * @param accessToken of user who logged in
+     */
+    public QuestionEntity createQuestion(QuestionEntity questionEntity, String accesstoken)
+                    throws AuthorizationFailedException {
+
+        UserAuthEntity userAuthEntity = userAuthDao.getUserAuthByToken(accesstoken);
+        if(userAuthEntity == null){
+            throw new AuthorizationFailedException("ATHR-001", "User has not signed in");
+        }
+
+        if(userAuthEntity.getLogoutAt()!=null){
+            throw new AuthorizationFailedException("ATHR-002",
+                    "User is signed out.Sign in first to post a question");
+        }
+
+        // Assign a UUID to the user that is being created.
+        questionEntity.setUuid(UUID.randomUUID().toString());
+        questionEntity.setUserEntity(userAuthEntity.getUserEntity());
+
+        return questionDao.createQuestion(questionEntity);
+    }
+
+    /**
+     * Get All QUESTION
+     * Signin before creating a question is mandatory
+     * @param accessToken of user who logged in
+     */
+//    public List<QuestionEntity> getAllQuestions(String accessToken) throws AuthorizationFailedException{
+//
+//        UserAuthEntity userAuthEntity = userAuthDao.getUserAuthByToken(accessToken);
+//        if(userAuthEntity == null){
+//            throw new AuthorizationFailedException("ATHR-001", "User has not signed in");
+//        }
+//
+//        if(userAuthEntity.getLogoutAt()!=null){
+//            throw new AuthorizationFailedException("ATHR-002",
+//                    "User is signed out.Sign in first to get all questions");
+//        }
+//
+//        List<QuestionEntity> questionEntity = questionDao.getAllQuestions();
+//
+//        return questionEntity;
+//    }
+}
