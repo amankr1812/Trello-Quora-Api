da810cbec18b9e97ebd5e735b6594d2b2eab7c7b
Dolly99 <67870449+Dolly99@users.noreply.github.com>
2020-08-15 16:35:59 +0530

    Added the getUserProfile functionality
    Signed-off-by: Dolly99 <67870449+Dolly99@users.noreply.github.com>

---
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/CommonController.java
@@ -0,0 +1,46 @@
+package com.upgrad.quora.api.controller;
+
+import com.upgrad.quora.api.model.UserDetailsResponse;
+import com.upgrad.quora.service.business.CommonService;
+import com.upgrad.quora.service.entity.UserEntity;
+import com.upgrad.quora.service.exception.AuthorizationFailedException;
+import com.upgrad.quora.service.exception.UserNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/")
+public class CommonController {
+
+    @Autowired
+    private CommonService commonService;
+
+    /**
+     * Get UserProfile - gets the user details
+     * Signin before getting user details is mandatory
+     * @param userUuid of user whose details you want to get
+     * @param accessToken(authorization) of signed in user
+     */
+    @RequestMapping(method = RequestMethod.GET, path = "/userprofile/{userId}",
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<UserDetailsResponse> userProfile(@PathVariable("userId") final String userUuid,
+                                                           @RequestHeader("authorization") final  String authorization) throws AuthorizationFailedException, UserNotFoundException {
+
+        String[] bearerToken = authorization.split("Bearer ");
+        UserEntity userEntity = commonService.userProfile(userUuid, bearerToken[1]);
+
+        UserDetailsResponse userDetailsResponse = new UserDetailsResponse().firstName(userEntity.getFirstName())
+                .lastName(userEntity.getLastName())
+                .userName(userEntity.getUserName())
+                .emailAddress(userEntity.getEmail())
+                .country(userEntity.getCountry())
+                .aboutMe(userEntity.getAboutMe())
+                .dob(userEntity.getDob())
+                .contactNumber(userEntity.getContactNumber());
+
+        return  new ResponseEntity<UserDetailsResponse>(userDetailsResponse, HttpStatus.OK);
+    }
+}
