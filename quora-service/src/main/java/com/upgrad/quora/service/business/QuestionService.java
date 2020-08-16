0c60dbc19f8f8d022ee1939d6194abac92b257f3
Aman Kumar <aman.kumar1812.svs@gmail.com>
2020-08-16 10:42:43 +0530

    Added Delete Question Service Code - Functionality Completed
    Signed-off-by: Aman Kumar <aman.kumar1812.svs@gmail.com>

--- quora-service/src/main/java/com/upgrad/quora/service/business/QuestionService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/QuestionService.java
@@ -131,7 +131,36 @@ public class QuestionService {
       return questionEntity;
     }
     
+    
+    /**
+     * DELETE A QUESTION
+     *
+     * @param accessToken of the signed in user
+     * @param questionId of the question to be edited.
+     */
+    @Transactional(propagation = Propagation.REQUIRED)
+    public QuestionEntity deleteQuestion(final String accessToken, final String questionId)
+        throws AuthorizationFailedException, InvalidQuestionException {
+      UserAuthEntity userAuthEntity = userAuthDao.getUserAuthByToken(accessToken);
+      if (userAuthEntity == null) {
+        throw new AuthorizationFailedException("ATHR-001", "User has not signed in");
+      } else if (userAuthEntity.getLogoutAt() != null) {
+        throw new AuthorizationFailedException(
+            "ATHR-002", "User is signed out.Sign in first to delete the question");
+      }
+      QuestionEntity questionEntity = questionDao.getQuestionById(questionId);
+      if (questionEntity == null) {
+        throw new InvalidQuestionException("QUES-001", "Entered question uuid does not exist");
+      }
+      if (!questionEntity.getUserEntity().getUuid().equals(userAuthEntity.getUserEntity().getUuid())
+          && !userAuthEntity.getUserEntity().getRole().equals("admin")) {
+        throw new AuthorizationFailedException(
+            "ATHR-003", "Only the question owner or admin can delete the question");
+      }
 
+      questionDao.deleteQuestion(questionEntity);
+      return questionEntity;
+    }
 
 
 }
